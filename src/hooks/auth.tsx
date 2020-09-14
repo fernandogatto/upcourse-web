8878a083ff5ad8047026976ca2be2315747e0164
fernandogatto <fernandogatto17@gmail.com>
2020-09-14 18:18:28 -0300

    Update auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,15 +1,21 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+const TOKEN_PREFIX = '@UpCourse:token';
+const TOKEN_TYPE_PREFIX = '@UpCourse:tokentype';
+const USER_PREFIX = '@UpCourse:user';
+
 interface IUser {
     id: string;
     name: string;
     email: string;
+    type: string;
     avatar_url: string;
 }
 
 interface IAuthState {
-    token: string;
+    access_token: string;
+    token_type: string;
     user: IUser;
 }
 
@@ -25,19 +31,23 @@ interface IAuthContextData {
     updateUser(user: IUser): void;
 }
 
+// Verifica se existe token para fazer o login
+const checkToken = () => localStorage.getItem(TOKEN_PREFIX) !== null;
+
 // Context
 const AuthContext = createContext<IAuthContextData>({} as IAuthContextData);
 
 // Component
 const AuthProvider: React.FC = ({ children }) => {
     const [authData, setAuthData] = useState<IAuthState>(() => {
-        const token = localStorage.getItem('@UpCourse:token');
-        const user = localStorage.getItem('@UpCourse:user');
+        const access_token = localStorage.getItem(TOKEN_PREFIX);
+        const token_type = localStorage.getItem(TOKEN_TYPE_PREFIX);
+        const user = localStorage.getItem(USER_PREFIX);
 
-        if(token && user) {
-            api.defaults.headers.authorization = `Bearer ${token}`;
+        if(token_type && access_token && user) {
+            api.defaults.headers.authorization = `${token_type} ${access_token}`;
 
-            return { token, user: JSON.parse(user) };
+            return { access_token, token_type, user: JSON.parse(user) };
         }
 
         return {} as IAuthState;
@@ -49,33 +59,40 @@ const AuthProvider: React.FC = ({ children }) => {
             password,
         });
 
-        const { token, user } = response.data;
+        const { access_token, token_type, user } = response.data;
 
-        if(response.data.status !== 'erro') {
-            localStorage.setItem('@UpCourse:token', token);
-            localStorage.setItem('@UpCourse:user', JSON.stringify(user));
+        // Só seta token e user se for diferente de undefined
+        if (typeof(access_token) !== 'undefined' &&
+            typeof(token_type) !== 'undefined' &&
+            typeof(user) !== 'undefined'
+        ) {
+            localStorage.setItem(TOKEN_PREFIX, access_token);
+            localStorage.setItem(TOKEN_TYPE_PREFIX, token_type);
+			localStorage.setItem(USER_PREFIX, JSON.stringify(user));
 
-            api.defaults.headers.authorization = `Bearer ${token}`;
+            api.defaults.headers.authorization = `${token_type} ${access_token}`;
 
-            setAuthData({ token, user });
+            setAuthData({ access_token, token_type, user });
         }
     }, []);
 
     const signOut = useCallback(() => {
-        localStorage.removeItem('@UpCourse:token');
-        localStorage.removeItem('@UpCourse:user');
+        localStorage.removeItem(TOKEN_PREFIX);
+		localStorage.removeItem(TOKEN_TYPE_PREFIX);
+		localStorage.removeItem(USER_PREFIX);
 
         setAuthData({} as IAuthState);
     }, []);
 
     const updateUser = useCallback((user: IUser) => {
-        localStorage.setItem('@UpCourse:user', JSON.stringify(user));
+        localStorage.setItem(USER_PREFIX, JSON.stringify(user));
 
         setAuthData({
-            token: authData.token,
+            access_token: authData.access_token,
+            token_type: authData.token_type,
             user,
         });
-    }, [setAuthData, authData.token]);
+    }, [setAuthData, authData.access_token, authData.token_type]);
 
     return (
         <AuthContext.Provider value={{
@@ -100,4 +117,4 @@ function useAuth(): IAuthContextData {
     return context;
 }
 
-export { AuthProvider, useAuth }
+export { AuthProvider, useAuth, checkToken }
